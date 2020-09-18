c8539d28015a0739343a986b416cf6b69d2dace9
Daniel Somoza Paletta <dasomoza@gmail.com>
2020-09-18 02:27:42 +0200

    [ADD] Added refresh access token  flow

--- src/http/interceptors/responseInterceptors.js
+++ src/http/interceptors/responseInterceptors.js
@@ -1,11 +1,43 @@
+import { HttpError } from "../errors/HttpError";
+import { generateAccessToken } from "../auth/auth";
+
 export function responseInterceptorSuccess(response) {
   return response.data;
 }
 
 export function responseInterceptorError(response) {
-  // TODO: IMPLEMENT HTTP ERROR
+  // TODO: NO INTERNET CONNECTION ERROR
 
-  // TODO: IMPLEMENT HERE REFRESH FLOW
+  const error = new HttpError(response.response);
+
+  const isAccessTokenExpired = error.isAccessTokenExpiredError();
+
+  if (isAccessTokenExpired) {
+    const isAccessTokenAlreadyCalled = this.isAccessTokenAlreadyCalled;
+
+    if (!isAccessTokenAlreadyCalled) {
+      this.generateAccessTokenRequest = generateAccessToken();
+      this.isAccessTokenAlreadyCalled = true;
+    }
+
+    return this.generateAccessTokenRequest
+      .then(({ access_token: accessToken }) => {
+        this.setAccessToken(accessToken);
+        this.isAccessTokenAlreadyCalled = false;
+        return retryCall(response, this);
+      })
+      .catch(() => {
+        this.isAccessTokenAlreadyCalled = false;
+        return Promise.reject(response.response);
+      });
+  }
 
   return Promise.reject(response.response);
+}
+
+function retryCall(error, Api) {
+  const { config } = error;
+  return Api.axiosInstance({
+    ...config,
+  });
 }
