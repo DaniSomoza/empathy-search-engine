c02c5241adf1ed9d6961c1cdbb128860e3e3a188
Daniel Somoza Paletta <dasomoza@gmail.com>
2020-09-19 19:32:46 +0200

    Updates

---
+++ precache-manifest.ca4e9f4992746821e22406cf48d6f25f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8e80c23717264a5746521b4afa594f",
+    "url": "/empathy-search-engine/index.html"
+  },
+  {
+    "revision": "2ad599002f9001aad785",
+    "url": "/empathy-search-engine/static/css/main.2f8f705a.chunk.css"
+  },
+  {
+    "revision": "934f9f4c2cd184233058",
+    "url": "/empathy-search-engine/static/js/2.a7674419.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/empathy-search-engine/static/js/2.a7674419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad599002f9001aad785",
+    "url": "/empathy-search-engine/static/js/main.863433cc.chunk.js"
+  },
+  {
+    "revision": "1e590947bcf55f82dcf7",
+    "url": "/empathy-search-engine/static/js/runtime-main.057e0836.js"
+  },
+  {
+    "revision": "1f8357142772b6b93e48572c466053e1",
+    "url": "/empathy-search-engine/static/media/arrow-drop-down.1f835714.svg"
+  },
+  {
+    "revision": "2c37daca6c50fd801ce3e57a9c6adac7",
+    "url": "/empathy-search-engine/static/media/search-icon.2c37daca.svg"
+  }
+]);
